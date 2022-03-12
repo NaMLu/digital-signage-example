d2ba9bc504fac11f0260f1badf021ee4cdb57823
Oğuzhan Şahin <osahin@outlook.com>
2022-03-12 17:20:51 +0300

    fix some eslint warnings

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,20 +1,17 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
--- server/src/services/mongodb/models/index.ts
+++ server/src/services/mongodb/models/index.ts
@@ -8,45 +8,47 @@ const db = {
   initial: (): void => {
     const Item = db.PlayListItemModel;
 
-    Item.estimatedDocumentCount((error: any, count: number) => {
-      if (!error && count === 0) {
-        new Item({
-          name: "Ice",
-          url: "https://cdn.shopify.com/s/files/1/2018/8867/files/ice.mp4",
-          type: "video",
-          duration: 8,
-        }).save((error: any) => {
-          if (error) {
-            console.log("error", error);
-          }
-          console.log("Added new Item to Items collection");
-        });
+    Item.estimatedDocumentCount(
+      (error: mongoose.CallbackError, count: number) => {
+        if (!error && count === 0) {
+          new Item({
+            name: "Ice",
+            url: "https://cdn.shopify.com/s/files/1/2018/8867/files/ice.mp4",
+            type: "video",
+            duration: 8,
+          }).save((error: mongoose.CallbackError) => {
+            if (error) {
+              console.log("error", error);
+            }
+            console.log("Added new Item to Items collection");
+          });
 
-        new Item({
-          name: "Random Image 1",
-          url: "https://loremflickr.com/1920/1080?random=1",
-          type: "image",
-          duration: 3,
-        }).save((error: any) => {
-          if (error) {
-            console.log("error", error);
-          }
-          console.log("Added new Item to Items collection");
-        });
+          new Item({
+            name: "Random Image 1",
+            url: "https://loremflickr.com/1920/1080?random=1",
+            type: "image",
+            duration: 3,
+          }).save((error: mongoose.CallbackError) => {
+            if (error) {
+              console.log("error", error);
+            }
+            console.log("Added new Item to Items collection");
+          });
 
-        new Item({
-          name: "Random Image 2",
-          url: "https://loremflickr.com/1920/1080?random=2",
-          type: "image",
-          duration: 3,
-        }).save((error: any) => {
-          if (error) {
-            console.log("error", error);
-          }
-          console.log("Added new Item to Items collection");
-        });
+          new Item({
+            name: "Random Image 2",
+            url: "https://loremflickr.com/1920/1080?random=2",
+            type: "image",
+            duration: 3,
+          }).save((error: mongoose.CallbackError) => {
+            if (error) {
+              console.log("error", error);
+            }
+            console.log("Added new Item to Items collection");
+          });
+        }
       }
-    });
+    );
   },
 };
 
