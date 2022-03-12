2cc1e68e79d9e2fc2819aeaef8875d03c7bc96fa
Oğuzhan Şahin <osahin@outlook.com>
2022-03-12 16:54:03 +0300

    update service with env

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,7 +1,12 @@
-// import PlayListItemMongodbService from "./mongodb/PlayListItemMongodbService";
+import PlayListItemMongodbService from "./mongodb/PlayListItemMongodbService";
 import PlayListItemLocalVariableService from "./localVariable/PlayListItemLocalVariableService";
 import { IPlayListItemService } from "../interfaces/play_list_item_service.interface";
+import dotenv from "dotenv";
+dotenv.config();
 
-const service: IPlayListItemService = new PlayListItemLocalVariableService();
+const service: IPlayListItemService =
+  process.env.ISMONGO == "true"
+    ? new PlayListItemMongodbService()
+    : new PlayListItemLocalVariableService();
 
 export default service;
